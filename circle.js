c195b40a42d7e98108af7f3cfd753e485e2fe743
mahnoorhusain <mahnooorhusain@gmail.com>
2025-04-28 22:36:25 +0500

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,5 @@
+const piValue = Math.PI
+let radius =  parseFloat (prompt(`enter the radius: `));
+alert (`the diameter of the circle is:  ${2*radius}`);
+alert (`the circumference of the circle is: ${2*piValue*radius}`);
+alert (`the area of the circle is: ${piValue*Math.pow(radius,2)}`); 
